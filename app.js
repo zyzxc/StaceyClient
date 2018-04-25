ad48e4817dcaf5a822be433ce1909af7a528dc1d
zxc <zhangxingcai@zysoft.com.cn>
2018-04-25 17:33:36 +0800

    jade模板改为html模板

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ejs = require('ejs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,7 +25,8 @@ app.all('*', function (req, res, next) {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
 
 app.get('/index', function (req, res) {
     res.render('indexRouter', {title: '首页'});
